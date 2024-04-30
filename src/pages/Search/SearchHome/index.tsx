19fdf88769a5ef164c8188d7fc2ed1e65afced18
kim <perfumeplaylist.gmail.com>
2024-04-30 14:20:32 +0900

    refactor: useKeyword 커스텀 훅으로 비지니스 로직 숨김

--- src/pages/Search/SearchHome/index.tsx
+++ src/pages/Search/SearchHome/index.tsx
@@ -1,10 +1,11 @@
-import { Suspense, useDeferredValue, useMemo, useState } from 'react';
+import { Suspense, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Outlet } from 'react-router-dom';
 import { Footer } from '@/shared/components';
 import { useGetSearchParams } from '@/shared/hooks';
 import { Container, Wrapper, HeaderBox, SearchContainer } from './style';
 import { SearchForm } from '@/features/search/components';
+import { useKeyword } from '@/features/search/hooks';
 
 const SearchHome = () => {
   const searchKeyword = useGetSearchParams('keyword');
@@ -13,15 +14,7 @@ const SearchHome = () => {
     searchKeyword ? decodeURIComponent(searchKeyword) : ''
   );
 
-  const deferredValue = useDeferredValue(keyword);
-
-  const lateKeyword = useMemo(() => {
-    if (!keyword.length && keyword === '') {
-      return keyword;
-    }
-
-    return deferredValue;
-  }, [keyword, deferredValue]);
+  const lateKeyword = useKeyword(keyword);
 
   const onInput = (word: string) => {
     setKeyword(word);
