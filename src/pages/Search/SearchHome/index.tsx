cb5ec3803d5b9a035215a21c574925ee8b10561b
kim <perfumeplaylist.gmail.com>
2024-04-30 14:09:39 +0900

    fix: 검색어 한번에 삭제시 검색 리스트 잠깐 보이는 오류 수정

--- src/pages/Search/SearchHome/index.tsx
+++ src/pages/Search/SearchHome/index.tsx
@@ -1,4 +1,5 @@
-import { Suspense, useDeferredValue, useState } from 'react';
+import { Suspense, useDeferredValue, useMemo, useState } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Outlet } from 'react-router-dom';
 import { Footer } from '@/shared/components';
 import { useGetSearchParams } from '@/shared/hooks';
@@ -12,8 +13,15 @@ const SearchHome = () => {
     searchKeyword ? decodeURIComponent(searchKeyword) : ''
   );
 
-  const lateKeyword = useDeferredValue(keyword);
-  // const lateKeyword = useMemo(() => deferredValue, [keyword]);
+  const deferredValue = useDeferredValue(keyword);
+
+  const lateKeyword = useMemo(() => {
+    if (!keyword.length && keyword === '') {
+      return keyword;
+    }
+
+    return deferredValue;
+  }, [keyword, deferredValue]);
 
   const onInput = (word: string) => {
     setKeyword(word);
@@ -26,9 +34,11 @@ const SearchHome = () => {
           <HeaderBox />
           <SearchForm keyword={keyword} onInput={onInput} />
           <SearchContainer>
-            <Suspense fallback={<>Loading...</>}>
-              <Outlet context={{ keyword: lateKeyword, onInput }} />
-            </Suspense>
+            <ErrorBoundary fallback={<>Error처리</>}>
+              <Suspense fallback={<>Loading...</>}>
+                <Outlet context={{ keyword: lateKeyword, onInput }} />
+              </Suspense>
+            </ErrorBoundary>
           </SearchContainer>
         </Wrapper>
       </Container>
