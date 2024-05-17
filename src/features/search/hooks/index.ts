981a73bd7db8d51ff7e11b267de73f8ddd4c2c69
kim <perfumeplaylist.gmail.com>
2024-05-17 13:24:04 +0900

    Revert "fix: 디바운스 파일 제거"
    This reverts commit 61126f59ebd92de5b89c3f4a3099763355b3be1f.

--- src/features/search/hooks/index.ts
+++ src/features/search/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useSearchedKeywordStorage } from './useSearchedKeywordStorage';
+export { default as useDebounce } from './useDebounce';
 export { default as useSearchFocus } from './useSearchFocus';
 export { default as useKeyword } from './useKeyword';
