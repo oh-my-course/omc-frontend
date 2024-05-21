8366c8ae3b6ec2741f63691952221e37e19b5c68
kim <perfumeplaylist.gmail.com>
2024-05-21 16:26:13 +0900

    fix: .yarml 으로 확장자 변경 및 lighthouserc.js 쓸대없는 속성 삭제

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './dist',
-      startServerCommand: 'npm run preview', // 서버를 키는 명령어를 통해서도 실행 가능
+      // startServerCommand: 'npm run preview', // 서버를 키는 명령어를 통해서도 실행 가능
       url: ['http://localhost:5173'],
       numberOfRuns: 3,
     },
@@ -16,12 +16,6 @@ module.exports = {
     },
     upload: {
       target: 'temporary-public-storage',
-    },
-    server: {
-      // server options here
-    },
-    wizard: {
-      // wizard options here
     },
   },
 };
