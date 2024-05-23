61fc6f8ec212223760fdaa81d9d20dcd8bacb5cd
kim <perfumeplaylist.gmail.com>
2024-05-23 15:09:50 +0900

    feat: 컨벤션에 맞는 export 추가

--- src/features/item/components/index.ts
+++ src/features/item/components/index.ts
@@ -1,2 +1,4 @@
 export { default as ListItem } from './ListItem';
 export { default as ItemComment } from './ItemComment';
+export { default as Item } from './Item';
+export { default as AddItem } from './AddItem';
