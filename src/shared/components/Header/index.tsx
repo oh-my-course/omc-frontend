960e7106fb0515207de74486941e2f04a81e985f
kim <perfumeplaylist.gmail.com>
2024-05-23 15:07:36 +0900

    feat: ui를 위한 header에 title 적용

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -1,15 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 import { Box, Flex, Image, Text } from '@chakra-ui/react';
-import { CommonIconButton } from '@/shared/components';
+import { CommonIconButton, CommonText } from '@/shared/components';
 import logo from '@/assets/images/logo.png';
 
 interface HeaderProps {
   type: 'logo' | 'back';
   path?: string;
   height?: string;
+  title?: string;
 }
 
-const Header = ({ type, path, height = '4rem' }: HeaderProps) => {
+const Header = ({ type, path, height = '4rem', title }: HeaderProps) => {
   const navigate = useNavigate();
 
   const headerType = {
@@ -22,11 +23,14 @@ const Header = ({ type, path, height = '4rem' }: HeaderProps) => {
       </>
     ),
     back: (
-      <CommonIconButton
-        type="back"
-        fontSize="1rem"
-        onClick={() => (path ? navigate(path) : navigate(-1))}
-      />
+      <>
+        <CommonIconButton
+          type="back"
+          fontSize="1rem"
+          onClick={() => (path ? navigate(path) : navigate(-1))}
+        />
+        <CommonText type="smallTitle">{title}</CommonText>
+      </>
     ),
   };
 
