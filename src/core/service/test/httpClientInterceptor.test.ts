4761b3ebbfbd4260852bfe7970b0456798eb8bcb
kim <perfumeplaylist.gmail.com>
2024-05-30 14:55:19 +0900

    feat: interceptor 테스트 코드 추가

--- src/core/service/test/httpClientInterceptor.test.ts
+++ src/core/service/test/httpClientInterceptor.test.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import testClient from './httpInterceptorStub.ts';
+import { Storage } from '@/shared/utils';
+import TestClient from './httpInterceptorStub.ts';
 
 interface obj {
   [key: string]: string;
@@ -23,16 +24,38 @@ jest.mock('@/shared/utils', () => {
   };
 });
 
-describe('axios interceptor에 대한 테스트', () => {
-  let http: typeof testClient;
-  let mock: MockAdapter;
+// memberApi mock할당
+// 해당 스코프에 있어야지 인식을 한다.
+// stub를 생성하여 mock함수 제거할수 있다.
 
-  beforeAll(() => {
-    http = testClient;
-    mock = new MockAdapter(http.test.getTestInstance());
+// jest.mock('@/features/member/service', () => {
+//   const postRefresh = jest.fn(() => {
+//     return new Promise((resolve) => resolve({ data: { accessToken: '123' } }));
+//   });
+
+//   return {
+//     ...jest.requireActual('@/features/member/service'),
+//     memberApi: {
+//       ...jest.requireActual('@/features/member/service/handler'),
+//       postRefresh,
+//     },
+//   };
+// });
+
+describe('axios interceptor에 대한 테스트', () => {
+  const http = new TestClient({
+    baseURL: 'test',
+    timeout: 15000,
+    withCredentials: true,
   });
 
-  afterAll(() => {
+  let mock: MockAdapter;
+
+  beforeEach(() => {
+    mock = new MockAdapter(http.getTestInstance());
+  });
+
+  afterEach(() => {
     mock.reset();
   });
 
@@ -42,7 +65,24 @@ describe('axios interceptor에 대한 테스트', () => {
     });
 
     // 토큰이 있을때
-    const res = await http.test.get<AxiosRequestConfig>('/test');
+    const res = await http.get<AxiosRequestConfig>('/test');
+    expect(Storage.getLocalStoraged).toHaveBeenCalledTimes(1);
     expect(res.headers!.Authorization).toBe('Bearer accessToken');
+  });
+
+  // stub 존재 이유
+  // 재요청을 보내면 끝나질 않는다
+  // 항상 에러가 터지기 때문에
+  // 이거를 극복하기 위해서는 한번 보내면 에러가 터지지 않도록 설정을 해줘야한다.
+
+  it('axios response interceptor error refresh success', async () => {
+    const errorBody = { code: 'COMMON_008' };
+    mock.onGet('/test-error').reply((config) => {
+      return [500, errorBody, config];
+    });
+
+    const res = await http.get<AxiosRequestConfig>('/test-error');
+    expect(Storage.setLocalStoraged).toHaveBeenCalledTimes(1);
+    expect(res!.headers?.Authorization).toBe(`Bearer 123`);
   });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { dependencies } from './package.json';
+
+function renderChunks(deps: Record<string, string>) {
+  const chunks: { [key: string]: string[] } = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) {
+      return;
+    }
+    chunks[key] = [key];
+  });
+
+  return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +25,16 @@ export default defineConfig({
         replacement: path.resolve(__dirname, 'src'),
       },
     ],
+  },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
   },
 });
