19275deba6d7fac5e5fe9e5c5b19c5d395b19976
kim <perfumeplaylist.gmail.com>
2024-06-03 11:09:33 +0900

    fix: vite config 번들링 최적화 로직 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,6 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { dependencies } from './package.json';
-
-function renderChunks(deps: Record<string, string>) {
-  const chunks: { [key: string]: string[] } = {};
-  Object.keys(deps).forEach((key) => {
-    if (['react', 'react-router-dom', 'react-dom'].includes(key)) {
-      return;
-    }
-    chunks[key] = [key];
-  });
-
-  return chunks;
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,16 +12,5 @@ export default defineConfig({
         replacement: path.resolve(__dirname, 'src'),
       },
     ],
-  },
-  build: {
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-router-dom', 'react-dom'],
-          ...renderChunks(dependencies),
-        },
-      },
-    },
   },
 });
