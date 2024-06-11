5187481104a927630539c3845a096a2985763059
kim <perfumeplaylist.gmail.com>
2024-06-11 18:08:17 +0900

    fix: 아이템 컴포넌트로 분리

--- src/pages/Item/List/index.tsx
+++ src/pages/Item/List/index.tsx
@@ -1,54 +1,25 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useInfiniteQuery } from '@tanstack/react-query';
-import { CommonIconButton, CommonText, Header, Footer, CommonSpinner } from '@/shared/components';
-import { useAuthNavigate, useIntersectionObserver } from '@/shared/hooks';
-import { Container, TitleContainer, AddContainer, ButtonBox, NoResult } from './style';
-import { Item } from '@/features/item/components';
+import {
+  CommonIconButton,
+  CommonText,
+  Header,
+  Footer,
+  SuspenseBoundary,
+  Skeleton,
+} from '@/shared/components';
+import { useAuthNavigate } from '@/shared/hooks';
+import { Container, TitleContainer, AddContainer, ButtonBox } from './style';
+import { ItemList } from '@/features/item/components';
 import { useDeleteItem } from '@/features/item/hooks';
-import { itemQueryOption } from '@/features/item/service';
 
-const ItemList = () => {
-  const navigate = useNavigate();
-
+const Item = () => {
   const [deleteData, setDeleteData] = useState<number[]>([]);
 
   const [isDelete, setIsDelete] = useState<boolean>(false);
 
   const authNavigate = useAuthNavigate();
 
-  const {
-    mutate: itemMutate,
-    isError: itemDeleteError,
-    isPending: itemDeletePending,
-  } = useDeleteItem({ cursorId: '', size: 3 });
-
-  const { data, hasNextPage, fetchNextPage, isPending, isError } = useInfiniteQuery({
-    ...itemQueryOption.infinityList({ size: 13 }),
-    select: (data) => {
-      return {
-        summaries: data.pages.flatMap(({ summaries }) => summaries),
-        totalCount: data.pages[0].totalMemberItemCount,
-      };
-    },
-  });
-
-  const ref = useIntersectionObserver({ onObserve: fetchNextPage });
-
-  const handleImageClick = (id: number) => {
-    if (isDelete) {
-      return;
-    }
-    navigate(`/item/${id}`);
-  };
-
-  const handleChange = (deleteId: number) => {
-    if (!deleteData.includes(deleteId)) {
-      setDeleteData((prev) => [...prev, deleteId]);
-    } else {
-      setDeleteData((prev) => [...prev.filter((id) => id !== deleteId)]);
-    }
-  };
+  const { mutate: itemMutate } = useDeleteItem({ cursorId: '', size: 3 });
 
   const handleClick = () => {
     setDeleteData([]);
@@ -61,18 +32,6 @@ const ItemList = () => {
     }
     handleClick();
   };
-
-  if (isPending || itemDeletePending) {
-    return (
-      <NoResult>
-        <CommonSpinner size="xl" />
-      </NoResult>
-    );
-  }
-
-  if (isError || itemDeleteError) {
-    return <NoResult>Error...</NoResult>;
-  }
 
   return (
     <>
@@ -89,29 +48,22 @@ const ItemList = () => {
             <CommonIconButton type="delete" onClick={() => setIsDelete((prev) => !prev)} />
           )}
         </TitleContainer>
-        <Item>
-          <Item.SubCountInfo>
-            {isDelete
-              ? `총 삭제할 아이템 ${deleteData.length}개`
-              : `총 아이템 ${data.totalCount}개`}
-          </Item.SubCountInfo>
-          <Item.ImageContainer>
-            {data.summaries.map(({ itemInfo: { id, image, name, price } }) => (
-              <Item.ImageBox key={id} isBlur={deleteData.includes(id)}>
-                <Item.ImageInput
-                  id={id}
-                  onChange={isDelete ? () => handleChange(id) : () => handleImageClick(id)}
-                />
-                <Item.ImageInputLabel id={id}>
-                  <Item.Image src={image} />
-                </Item.ImageInputLabel>
-                <Item.Price price={price} />
-                <Item.Title name={name} limit={20} />
-              </Item.ImageBox>
-            ))}
-            {hasNextPage && <div ref={ref} />}
-          </Item.ImageContainer>
-        </Item>
+        <SuspenseBoundary
+          suspenseFallback={
+            <Skeleton>
+              <Skeleton.TextWrapper>
+                <Skeleton.Text />
+              </Skeleton.TextWrapper>
+              <Skeleton.GridWrapper column={3} rows={4}>
+                <Skeleton.GridItem count={11}>
+                  <Skeleton.Item />
+                </Skeleton.GridItem>
+              </Skeleton.GridWrapper>
+            </Skeleton>
+          }
+        >
+          <ItemList isDelete={isDelete} deleteData={deleteData} setDeleteData={setDeleteData} />
+        </SuspenseBoundary>
       </Container>
       <Footer>
         <AddContainer>
@@ -122,4 +74,4 @@ const ItemList = () => {
   );
 };
 
-export default ItemList;
+export default Item;
